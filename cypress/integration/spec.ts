36cf31dde317c6ab4903b2a0847391ac0e876154
Anastasiya.Golova <Anastasiya.Golova@waveaccess.ru>
2022-12-19 00:39:10 +0400

    Update spec.ts

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,5 +1,17 @@
-describe('Counter (with helpers)', () => {
-  beforeEach(() => {
-    cy.visit('/viewRss');
-  });
+describe('Просмотр rss-лент', () => { 
+  it('на главной странице должен отображаться заголовок', () => { 
+    cy.visit('/viewRss' ); // переходим на страницу 
+    cy.get('.tittle').should('have.text', 'Добавленные rss-ленты');
+
+    cy.get('.new-btn').click(); 
+    cy.get('.form-control > :nth-child(1)').click();
+    cy.get('#url').type('www.example.com').should('have.value', 'www.example.com');
+    cy.get('.btn-success').click();
+  }); 
+  it('создание новой rss-ленты', () => { 
+    cy.get('.new-btn').click(); 
+    cy.get('.form-control > :nth-child(1)').click();
+    cy.get('#url').type('www.example.com').should('have.value', 'www.example.com');
+    cy.get('.btn-success').click();
+  }); 
 });
