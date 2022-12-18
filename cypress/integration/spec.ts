4372f835ab2d24fd1ff65966359c6386a3e03609
Anastasiya.Golova <Anastasiya.Golova@waveaccess.ru>
2022-12-19 01:02:11 +0400

    Update spec.ts

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,8 +1,23 @@
+describe('О сервисе', () => { 
+  it('на главной странице должен отображаться заголовок', () => { 
+    cy.visit('/' );  
+    cy.get('.tittle').should('have.text', 'О сервисе');
+
+  }); 
+});
+
 describe('Просмотр rss-лент', () => { 
   it('на главной странице должен отображаться заголовок', () => { 
     cy.visit('/viewRss' );  
     cy.get('.tittle').should('have.text', 'Добавленные rss-ленты');
 
+  }); 
+  it('получить название заголовков таблицы', () => {  
+    cy.get('thead > tr > :nth-child(1)').should('have.text', 'Текущая позиция');
+    cy.get('thead > tr > :nth-child(2)').should('have.text', 'Url');
+    cy.get('thead > tr > :nth-child(3)').should('have.text', 'Дата добавления');
+    cy.get('thead > tr > :nth-child(4)').should('have.text', 'Дата обновления');
+    cy.get('thead > tr > :nth-child(5)').should('have.text', 'Вкл/Выкл ленту');
   }); 
   it('создание новой rss-ленты', () => { 
     cy.get('.new-btn').click(); 
@@ -20,6 +35,12 @@ describe('Просмотреть новости', () => {
     cy.visit('/viewNews' );  
     cy.get('.tittle').should('have.text', 'Новости');
 
+  }); 
+  it('получить название заголовков таблицы', () => {  
+    cy.get('thead > tr > :nth-child(1)').should('have.text', 'Заголовок');
+    cy.get('thead > tr > :nth-child(2)').should('have.text', 'Ссылка');
+    cy.get('thead > tr > :nth-child(3)').should('have.text', 'Описание');
+    cy.get('thead > tr > :nth-child(4)').should('have.text', 'Дата публикации');
   }); 
 });
 
