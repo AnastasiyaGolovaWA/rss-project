edbea90ca8a59ad51959e42da4f2b2ce363dc9a0
Anastasiya.Golova <Anastasiya.Golova@waveaccess.ru>
2022-12-19 00:47:07 +0400

    Update spec.ts

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,17 +1,16 @@
 describe('Просмотр rss-лент', () => { 
   it('на главной странице должен отображаться заголовок', () => { 
-    cy.visit('/viewRss' ); // переходим на страницу 
+    cy.visit('/viewRss' );  
     cy.get('.tittle').should('have.text', 'Добавленные rss-ленты');
 
-    cy.get('.new-btn').click(); 
-    cy.get('.form-control > :nth-child(1)').click();
-    cy.get('#url').type('www.example.com').should('have.value', 'www.example.com');
-    cy.get('.btn-success').click();
   }); 
   it('создание новой rss-ленты', () => { 
     cy.get('.new-btn').click(); 
     cy.get('.form-control > :nth-child(1)').click();
     cy.get('#url').type('www.example.com').should('have.value', 'www.example.com');
     cy.get('.btn-success').click();
+  }); 
+  it('удаление rss-ленты', () => { 
+    cy.get(':nth-child(5) > :nth-child(6) > .btn-class').click(); 
   }); 
 });
